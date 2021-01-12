526adcf6dcd9ddd9ff458988429e415d238e4cac
Reinhard Weber <extern.weber_reinhard1@allianz.com>
2021-01-12 14:39:18 +0100

    Inital version of the plugin

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# dynatrace-plugin-certcheck
+# A Dynatrace Remote Plugin to Monitor SSL Certificate Expiry
+This active gate remote plugin enhances Dynatrace synthetic monitoring capabilities by checking the expiry date of SSL certificates.
+It overcomes the limitation by executing checks against the endpoints that are already configured in Dynatrace's synthetic tests and will post any problems to these synthetic test entities.
+
+## Advantages
+Compared to other solutions, this plugin doesn't need any extra configuration for the sites it will check. It reuses the already configured synthetic monitors to perform it's checks, so there is no need to perform extra configuration on the plugin side. Whenever synthetic monitors are added (and potentially tagged) the plugin will check their site's certificate.
+
+Also this plugin will not consume any licenses for custom metrics or DEM points, as it doesn't perform any extra synthetic requests or create metrics.
+
+## How to use
+To use the plugin you will need an active gate that has the remoteplugin module installed (default for new active gates) and that is able to access the sites you want to monitor.
+The plugin works by contacting the Dynatrace API to fetch all configured synthetic montiors (currently HTTP monitors only). It then determines the hosts that are configured in the synthetic requests and uses this information to perform an SSL validation check.
+
+The plugin requires access to the Dynatrace API so you will need an API key with these permissions:
+- Access problem and event feed, metrics, and topology
+- DataIngest, e.g. metrics and events
+- Read synthetic monitors, locations, and nodes
+
+### Configuration Options
+These configuration options are available for the plugin:
+
+- Minimum Certificate Validity in Days: the threshold when the plugin should create an alert for a certificate that is about to expire
+- Time interval for checks: the interval at which the plugin should check certificates (it makes no sense to check every minute). Please note that the interval is calculated based on the current time and not the configuration time. E.g. "every 15 minutes" will mean the check is performed at the full hour, 15 past, 30 past, 45 past the full hour. Likewise every "2 hours" means at 2am, 4am, ... 12am. 
+- Select Synthetic Monitors by Tag: Limit the certificate check to synthetic HTTP monitors that are tagged with this tag. Only one tag is allowed.
+- Dynatrace Tenant UUID to report to: this is the tenant UUID that the plugin will get synthetic monitors from and report to (using the configured API token). Typically this will be the same tenant as the plugin is configured for. Only provide the tenant ID, not the full hostname.
+- Dynatrace API token: the API token with the permissions mentioned above
+
+## Further Options
+As the plugin as acces to the full certificate it would be rather easy to perform also other checks (e.g. hostname matches). This might be a future addition.
---
+++ certcheck_plugin/certcheck.py
@@ -0,0 +1,219 @@
+'''
+    Dynatrace SSL Certrificate Checker Active Gate Plugin
+    This plugin enhances Dynatrace synthetic monitoring by adding a SSL certificate validation that checks for the expiry date of certificates
+
+    The plugin works by executing on an Active Gate, fetching all (or tagged) HTTP synthetic monitors
+    from the environment and then validating their certificates.
+    If a certificate expires within the given amount of days the plugin will post an error event to Dynatrace.
+
+'''
+#!/usr/bin/env python
+__author__ = "Reinhard Weber"
+__copyright__ = "Copyright 2021, 360 Performance GmbH"
+__credits__ = ["Reinhard Weber"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Reinhard Weber"
+__email__ = "r.weber@360performance.net"
+__status__ = "Production"
+
+from ruxit.api.base_plugin import RemoteBasePlugin
+from ruxit.api.exceptions import ConfigException
+import requests, urllib3, json
+import logging,sys
+from urllib.parse import urlencode, urlparse
+from datetime import datetime, timedelta
+
+from OpenSSL import SSL
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+import idna
+
+from socket import socket
+from collections import namedtuple
+
+HostInfo = namedtuple(field_names='cert hostname peername', typename='HostInfo')
+datefmt = "%Y-%m-%d %H:%M:%S"
+
+logger = logging.getLogger(__name__)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class CertificateCheckPlugin(RemoteBasePlugin):
+
+    def initialize(self, **kwargs):
+        logger.info("Config: %s", self.config)
+        self.minduration = self.config["minduration"]
+        self.tenant = self.config["tenantUUID"]
+        self.apitoken = self.config["apitoken"]
+        self.tag = self.config["tag"]
+        self.interval = self.config["interval"]
+        self.server = "https://localhost:9999/e/"+self.tenant   # this is an active gate plugin so it can call the DT API on localhost
+        self.problemtimeout = 15
+
+
+    def query(self, **kwargs):
+        config = kwargs['config']
+        group_name = "SSL Certificate Hosts"
+        group = self.topology_builder.create_group(group_name, group_name)
+
+        # determine if we should run the checks based on selected interval
+        time_minute = datetime.now().minute
+        time_hour = datetime.now().hour
+        if "hour" in self.interval:
+            hour = int(self.interval.strip().split()[0])
+            run = (time_hour%hour == 0) and time_minute == 0
+            self.problemtimeout = hour
+        if "minute" in self.interval:
+            minute = int(self.interval.strip().split()[0])
+            run = (time_minute%minute == 0)
+            self.problemtimeout = minute
+        
+        logger.info("Set to run every {}, it is now {:02d}:{:02d}. Check will{}run!".format(self.interval,time_hour, time_minute, " " if run else " not "))
+        
+        if run:
+            # Get Hosts from Already Configured Synthetic Monitors of this environment
+            hosts = self.getSSLCheckHosts(self.getSyntheticMonitors())
+            #logger.info(hosts)
+            self.getCertExpiry(hosts)
+
+
+    def getSyntheticMonitors(self):
+        apiurl = "/api/v1/synthetic/monitors"
+        parameters = {"tag":self.tag, "enabled":"true"}
+        query = "?"+urlencode(parameters)
+        headers = {"Authorization": "Api-Token {}".format(self.apitoken)}
+        url = self.server + apiurl + query
+
+        monitors = {}
+        try:
+            response = requests.get(url, headers=headers, verify=False)
+            result = response.json()
+
+            for monitor in result["monitors"]:
+                if monitor['type'] == "HTTP":
+                    monitors.update({monitor["name"]:monitor["entityId"]})
+        except:
+            logger.error("Error while trying to get synthetic monitors for {}::{}".format(clusterid,tenantid))
+
+        #logger.info(monitors)
+        return monitors
+
+    def getSSLCheckHosts(self, monitors):
+        apiurl = "/api/v1/synthetic/monitors/:id"
+        #parameters = {"clusterid":clusterid}
+        #query = "?"+urlencode(parameters)
+        headers = {"Authorization": "Api-Token {}".format(self.apitoken)}
+        url = self.server + apiurl #+ query
+
+        hosts = {}
+        session = requests.session()
+        #session.auth = (apiuser, apipwd)
+        session.verify = False
+        session.headers = headers
+        for m_name,m_id in monitors.items():
+            try:
+                m_url = url.replace(':id',m_id)
+                response = session.get(m_url)
+                result = response.json()
+
+                m_requests = result["script"]
+                for req in m_requests["requests"]:
+                    parsed = urlparse(req["url"])
+                    if parsed.scheme == "https":
+                        hosts.update({"{}://{}{}".format(parsed.scheme, parsed.hostname, "" if not parsed.port else ":"+str(parsed.port)) : m_id})
+            except:
+                logger.error("Error while trying to get synthetic hosts") 
+        
+        return hosts
+
+    def get_certificate(self, hostname, port):
+        hostname_idna = idna.encode(hostname)
+        sock = socket()
+
+        sock.connect((hostname, port))
+        peername = sock.getpeername()
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        ctx.check_hostname = False
+        ctx.verify_mode = SSL.VERIFY_NONE
+
+        sock_ssl = SSL.Connection(ctx, sock)
+        sock_ssl.set_connect_state()
+        sock_ssl.set_tlsext_host_name(hostname_idna)
+        sock_ssl.do_handshake()
+        cert = sock_ssl.get_peer_certificate()
+        crypto_cert = cert.to_cryptography()
+        sock_ssl.close()
+        sock.close()
+
+        return HostInfo(cert=crypto_cert, peername=peername, hostname=hostname)
+
+    def get_alt_names(self, cert):
+        try:
+            ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
+            return ext.value.get_values_for_type(x509.DNSName)
+        except x509.ExtensionNotFound:
+            return None
+
+    def get_common_name(self, cert):
+        try:
+            names = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+            return names[0].value
+        except x509.ExtensionNotFound:
+            return None
+
+    def get_issuer(self, cert):
+        try:
+            names = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
+            return names[0].value
+        except x509.ExtensionNotFound:
+            return None
+
+    def getCertExpiry(self, hosts):
+        for host, monitor_id in hosts.items():
+            parsed = urlparse(host)
+            hostinfo = self.get_certificate(parsed.hostname, int(parsed.port) if parsed.port else 443)
+            notafter=hostinfo.cert.not_valid_after
+
+            now = datetime.now()
+            expires = (notafter - now).days
+            logger.info("Certificate for {} (MonitorID: {}) expires in {} days: {}".format(parsed.hostname, monitor_id, expires, "ERROR" if expires < self.minduration else "OK"))
+            
+            if expires < self.minduration:
+                self.reportCertExpiryEvent(hostinfo, expires, monitor_id)
+
+    def reportCertExpiryEvent(self, hostinfo, expires, monitor_id):
+        start = datetime.now()
+        end = start + timedelta(minutes=5)
+        notbefore = hostinfo.cert.not_valid_before
+        notafter = hostinfo.cert.not_valid_after
+        event = {
+                    "eventType": "ERROR_EVENT",
+                    "start": int(datetime.timestamp(start)*1000),
+                    "end": int(datetime.timestamp(end)*1000),
+                    "timeoutMinutes": self.problemtimeout,
+                    "description": "The SSL certificate for {} will expire in {} days!".format(hostinfo.hostname, expires),
+                    "attachRules": { "entityIds": [ monitor_id ] },
+                    "source": "Certificate Checker Active Gate Plugin",
+                    "customProperties": {
+                        "CommonName": self.get_common_name(hostinfo.cert),
+                        "Issuer": self.get_issuer(hostinfo.cert),
+                        "NotBefore": notbefore.strftime(datefmt),
+                        "NotAfter": notafter.strftime(datefmt)
+                    },
+                    "allowDavismerge": "false"
+                }
+        
+        apiurl = "/api/v1/events"
+        headers = {"Content-type": "application/json", "Authorization": "Api-Token {}".format(self.apitoken)}
+        url = self.server + apiurl
+
+        data = json.dumps(event)
+        try:
+            response = requests.post(url, json=event, headers=headers, verify=False)
+            logger.info(response.json())
+        except:
+            logger.error("There was a problem posting error event to Dynatrace!")
+
+
+
+    
